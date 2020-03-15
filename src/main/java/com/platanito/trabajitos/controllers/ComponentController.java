ee36471ecb11eb64b6b05ab1add39c100c541c17
Daniel Pacheco <imdevdaniel@gmail.com>
2020-03-14 23:06:09 -0600

    Adds annotations for request body (save) and path variable (delete, searchById)

--- src/main/java/com/platanito/trabajitos/controllers/ComponentController.java
+++ src/main/java/com/platanito/trabajitos/controllers/ComponentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,18 +26,18 @@ public class ComponentController {
 		return (List<Component>) componentService.findAll() ;
 	}
 	
-	@RequestMapping(value="{id}", method=RequestMethod.GET)
-	public Optional<Component> findById(Long id) {
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public Optional<Component> findById(@PathVariable("id") Long id) {
 		return componentService.findById(id);
 	}
 	
 	@RequestMapping(value="", method=RequestMethod.POST)
-	public Component save(Component entity) {
+	public Component save(@RequestBody Component entity) {
 		return componentService.save(entity);
 	}
 	
-	@RequestMapping(value="{id}", method=RequestMethod.DELETE)
-	public void deleteById(Long id) {
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public void deleteById(@PathVariable("id") Long id) {
 		componentService.deleteById(id);
 	}
 
